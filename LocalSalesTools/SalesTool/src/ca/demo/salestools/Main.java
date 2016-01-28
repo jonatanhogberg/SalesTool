4d5e6023ba40722c2e1ec987e02716216fa2acb3
jonatanhogberg <ndi14jhg@student.hig.se>
2016-01-28 14:43:53 +0100

    Initial checkin of sales tool.

---
+++ LocalSalesTools/SalesTool/src/ca/demo/salestools/Main.java
@@ -0,0 +1,18 @@
+package ca.demo.salestools;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SalesData data = new SalesData();
+		
+		displayGreeting();
+		data.display();
+		
+	}
+	
+	private static void displayGreeting() {
+		System.out.println("Hello happy sales people!");
+		System.out.println("this app shows sales data");
+	}
+
+}
